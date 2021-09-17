21f9daa2e4962ac3c8358b389853f6b16c9bf9a5
Albart Jose <albartbtme@gmail.com>
2021-09-17 09:20:29 +0530

    Update

---
+++ src/Components/Employee.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+var initValue = {
+  name: "",
+  email: "",
+  age: "",
+  isMarried: null,
+};
+
+export const Employee = () => {
+  const [formData, setFormData] = useState(initValue);
+  const handleChange = (e) => {
+    const { name, value, checked, type } = e.target;
+    setFormData({ ...formData, [name]: type === "checkbox" ? checked : value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name
+        <input onChange={handleChange} type="text" name="name"></input>
+      </label>
+      <label>
+        email
+        <input onChange={handleChange} type="text" name="email"></input>
+      </label>
+      <label>
+        Age
+        <input onChange={handleChange} type="number" name="age"></input>
+      </label>
+      <label>
+        Married
+        <input onChange={handleChange} type="checkbox" name="isMarried"></input>
+      </label>
+      <label>Continent: </label>
+      <select onChange={handleChange} name="continent">
+        <option value="Asia">Asia</option>
+        <option value="Africa">Africa</option>
+        <option value="North America">North America</option>
+        <option value="South America">South America</option>
+        <option value="Europe">Europe</option>
+        <option value="Australia">Australia</option>
+      </select>
+      <input type="submit" value="submit"></input>
+    </form>
+  );
+};
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,31 +1,16 @@
 import React from "react";
 import "./Main.css";
 import { Counter } from "./Counter";
-import { Form } from "./Forms";
 
 function Main() {
-  // const [working, setWorking] = React.useState(false);
-  // const fileRef = React.useRef();
-
-  // function handleClickDisp() {
-  //   console.log(fileRef.current.files);
-  // }
-
   return (
     <div className="mainDiv">
-      {/* {!working ? <h3>Is it Working</h3> : <h3>Working</h3>}
-      <button
-        onClick={() => {
-          setWorking(!working);
-        }}
-      >
-        Test
-      </button>
-      <input type="file" ref={fileRef} />
-
-      <button onClick={handleClickDisp}>Show File in console</button> */}
-
       <Counter />
+
+      <br />
+      <br />
+      <br />
+
       <hr />
     </div>
   );
